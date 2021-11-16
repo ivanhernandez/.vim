c04a7f91143b70dd14551179814ca5c3a3dc4a3f
Ivan Hernandez <ivanhernandez.ihv@gmail.com>
2021-11-16 16:34:11 +0100

    Sumneko Lua server

--- lsp_config.lua
+++ lsp_config.lua
@@ -9,22 +9,79 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 
+
+-- SUMNEKO --------------------------------------------------------------------------------------
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+
+local sumneko_root_path = vim.fn.stdpath('config')..'/../sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+lspconfig.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+--- -----------------------------------------------
+
+
+
 lspconfig.bashls.setup{}
+
 lspconfig.cssls.setup { 
     capabilities = capabilities,
     cmd = { "vscode-css-language-server.cmd", "--stdio" }
 }
+
 --lspconfig.denols.setup{}
+
 --lspconfig.dockerls.setup{}
+
 lspconfig.elmls.setup{
     cmd = { "elm-language-server.cmd" }
 }
+
 fluttertools.setup{}
+
 lspconfig.gopls.setup{}
+
 lspconfig.html.setup { 
     capabilities = capabilities,
     cmd = { "vscode-html-language-server.cmd", "--stdio" }
 }
+
 lspconfig.jsonls.setup {
     cmd = { "vscode-json-languageserver.cmd", "--stdio" }
     -- commands = {
@@ -35,26 +92,20 @@ lspconfig.jsonls.setup {
     --   }
     -- }
 }
-nlualsp.setup(lspconfig, {
-  on_attach = function() end,
 
-  -- Include globals you want to tell the LSP are real :)
-  globals = {
-    -- Colorbuddy
-    "Color", "c", "Group", "g", "s",
-  }
-})
 lspconfig.nimls.setup{}
 --local omnisharp_bin = "/path/to/omnisharp/OmniSharp.exe"
 --lspconfig.omnisharp.setup{
 --    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
 --}
+
 lspconfig.rescriptls.setup {
     cmd = { "node", home .. "/.vim/plugged/vim-rescript/server/out/server.js", "--stdio" },
     -- on_attach = function(client, bufnr)
     --     require "lsp_signature".on_attach()
     -- end,
 }
+
 -- lspconfig.rls.setup {
 --   settings = {
 --     rust = {
@@ -64,20 +115,37 @@ lspconfig.rescriptls.setup {
 --     },
 --   },
 -- }
+
 lspconfig.rust_analyzer.setup{}
+
 lspconfig.svelte.setup{
     cmd = { "svelteserver.cmd", "--stdio" }
 }
+
 lspconfig.tsserver.setup{}
+
 lspconfig.vimls.setup{}
+
 lspconfig.vls.setup{
     cmd = { "vls.exe", "" }
 }
+
 lspconfig.yamlls.setup{
     cmd = { "yaml-language-server.cmd", "--stdio" }
 }
+
 lspconfig.zls.setup{
     cmd = { "zls.exe", "" }
 }
 
 
+
+nlualsp.setup(lspconfig, {
+  on_attach = function() end,
+
+  -- Include globals you want to tell the LSP are real :)
+  globals = {
+    -- Colorbuddy
+    "Color", "c", "Group", "g", "s",
+  }
+})
